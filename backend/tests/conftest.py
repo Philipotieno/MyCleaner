0de568234c40cdbd6ccb9241cfefbb14f4e5d4b3
Philip Otieno <iamphilipotieno@gmail.com>
2023-06-14 09:01:06 +0300

    tests: set up testing environment

---
+++ backend/tests/conftest.py
@@ -0,0 +1,45 @@
+import os
+import warnings
+
+import alembic
+import pytest
+from alembic.config import Config
+from asgi_lifespan import LifespanManager
+from databases import Database
+from fastapi import FastAPI
+from httpx import AsyncClient
+
+
+#Appy migration at beginning and end of testing session
+@pytest.fixture(scope="session")
+def apply_migrations():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    os.environ["TESTING"] = "1"
+
+    config = Config("alembic.ini")
+    alembic.command.upgrade(config, "head")
+    yield
+    alembic.command.downgrade(config, "base")
+
+#Create a new application for testing
+@pytest.fixture
+def app(apply_migrations: None) -> FastAPI:
+    from api.api.server import get_application
+
+    return get_application
+
+# Grab a reference to our database when necessary
+@pytest.fixture
+def db(app: FastAPI) -> Database:
+    return app.state._db
+
+# Mamke requests in our tests
+@pytest.fixture
+async def client(app: FastAPI) -> AsyncClient:
+    async with LifespanManager(app):
+        async with AsyncClient(
+            app=app,
+            base_url="http://testserver",
+            headers={"Content-Type": "application/json"}
+        ) as client:
+            yield client
